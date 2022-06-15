bc17ef385b647c4427f545374832c8ef6785157c
Mofferator <moffmoffjr@gmail.com>
2022-06-15 12:27:57 -0400

    Added "$bradface" and the ability to check for embed permissions

--- bradbot.py
+++ bradbot.py
@@ -21,7 +21,11 @@ with open("{}image_urls.json".format(PATH_PREFIX)) as urls:
 	image_urls = json.load(urls)
 	print("{} brad faces loaded".format(len(image_urls)), flush=True)
 
-
+def hasPerms(member, perms):
+  for role in reversed(member.roles):
+    if not perms.is_subset(role.permissions):
+        return False
+  return True
 
 @client.event
 async def on_ready():
@@ -40,6 +44,10 @@ async def on_message(message):
 		sent = await message.channel.send(line[0])
 		messagedb.addMessage(sent.id, sent.guild.id, sent.guild.name, line[0], line[1], message.author.id, message.author.name)
 
+	if message.content.startswith("$bradface"):
+		url = random.choice(image_urls)
+		await message.channel.send(url)
+
 @client.event
 async def on_guild_join(guild):
 	print("BradBot joined server: '{}'".format(guild.name), flush=True)
@@ -47,12 +55,15 @@ async def on_guild_join(guild):
 @client.event
 async def on_reaction_add(reaction, user):
 	if reaction.message.author == client.user:
-		message_id = reaction.message.id
-		msg_info = messagedb.getMessageInfo(message_id)
-		if msg_info != [] and len(reaction.message.reactions) == 1 and reaction.message.reactions[0].count == 1:
-			embed=discord.Embed(title="Match {}".format(msg_info[0][4]), url="https://opendota.com/matches/{}/chat".format(msg_info[0][4]), color=0xff0000)
-			embed.set_thumbnail(url=random.choice(image_urls))
-			print("%-25s %-35s Match ID fetched:'%s'" %(reaction.message.guild.name, "N/A", msg_info[0][4]), flush=True)
-			await reaction.message.reply(embed=embed)
+		if(reaction.message.channel.guild.me.guild_permissions.embed_links):
+			message_id = reaction.message.id
+			msg_info = messagedb.getMessageInfo(message_id)
+			if msg_info != [] and len(reaction.message.reactions) == 1 and reaction.message.reactions[0].count == 1:
+				embed=discord.Embed(title="Match {}".format(msg_info[0][4]), url="https://opendota.com/matches/{}/chat".format(msg_info[0][4]), color=0xff0000)
+				embed.set_thumbnail(url=random.choice(image_urls))
+				print("%-25s %-35s Match ID fetched:'%s'" %(reaction.message.guild.name, "N/A", msg_info[0][4]), flush=True)
+				await reaction.message.reply(embed=embed)
+		else:
+			await reaction.message.reply(msg_info[0][4])
 
 client.run(TOKEN)
