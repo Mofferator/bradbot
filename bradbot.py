ebe3b97603d865281e8cd7d43b0752b29ff0c9a6
Mofferator <moffmoffjr@gmail.com>
2022-06-17 13:31:29 -0400

    Added a help message to describe bradbot's functions

--- bradbot.py
+++ bradbot.py
@@ -8,7 +8,7 @@ else:
 	PATH_PREFIX = "/home/Mofferator/BradBot/bradbot/"
 
 load_dotenv('{}.env'.format(PATH_PREFIX))
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("TESTTOKEN")
 
 
 client = discord.Client()
@@ -21,11 +21,23 @@ with open("{}image_urls.json".format(PATH_PREFIX)) as urls:
 	image_urls = json.load(urls)
 	print("{} brad faces loaded".format(len(image_urls)), flush=True)
 
-def hasPerms(member, perms):
-  for role in reversed(member.roles):
-    if not perms.is_subset(role.permissions):
-        return False
-  return True
+def bradbot(message):
+	line = random.choice(words)
+	author = message.author.name
+	server = message.guild.name
+	print("%-25s %-35s Brad said:'%s'" %(server, author, line[0]), flush=True)
+	return line
+
+def bradface(message):
+	return random.choice(image_urls)
+
+def bradhelp(message):
+	embed=discord.Embed(title="Brad Bot Help", url="https://github.com/Mofferator/bradbot", color=0xff0000)
+	embed.set_thumbnail(url=random.choice(image_urls))
+	embed.add_field(name="Brad Bot", value="Type `bradbot` to make Brad Bot say one of his {} all chat lines at random".format(len(words)), inline=False)
+	embed.add_field(name="Brad Bot React", value="React to any of Brad Bot's all chat lines to get a link to the match", inline=False)
+	embed.add_field(name="Brad Face", value="Type `$bradface` to see a random picture of Bradley Dragon", inline=False)
+	return embed
 
 @client.event
 async def on_ready():
@@ -37,16 +49,16 @@ async def on_message(message):
 		return
 
 	if message.content.startswith("$bradbot"):
-		line = random.choice(words)
-		author = message.author.name
-		server = message.guild.name
-		print("%-25s %-35s Brad said:'%s'" %(server, author, line[0]), flush=True)
-		sent = await message.channel.send(line[0])
-		messagedb.addMessage(sent.id, sent.guild.id, sent.guild.name, line[0], line[1], message.author.id, message.author.name)
+		msg = bradbot(message)
+		sent = await message.channel.send(msg[0])
+		messagedb.addMessage(sent.id, sent.guild.id, sent.guild.name, msg[0], msg[1], message.author.id, message.author.name)
 
 	if message.content.startswith("$bradface"):
-		url = random.choice(image_urls)
-		await message.channel.send(url)
+		msg = bradface(message)
+		await message.channel.send(msg)
+
+	if message.content.startswith("$bradhelp"):
+		await message.reply(embed=bradhelp(message))
 
 @client.event
 async def on_guild_join(guild):
